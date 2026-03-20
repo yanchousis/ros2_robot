ae4c22c1b1238ef21e527c4c80054ed1f3bfd854
bip <none>
2026-03-20 13:06:15 +0300

    rm timer_node; add launch for metapackage; add timer_shutdown

---
+++ src/ros2_robot/launch/ros2_robot.launch.py
@@ -0,0 +1,63 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.actions import TimerAction, Shutdown, LogInfo
+
+def generate_launch_description():
+    time_work = 10.0 # ПОМЕНЯТЬ
+
+    pkg_share = get_package_share_directory('ros2_robot_description')
+    xacro_file = os.path.join(pkg_share, 'urdf', 'ros2_robot.urdf.xacro')
+    rviz_config_file = os.path.join(pkg_share, 'rviz', 'urdf.rviz')
+
+    # 1) Генерируем Substitution для xacro -> urdf
+    robot_description_substitution = Command(['xacro', xacro_file])
+    # 2) Явно указываем, что это строка
+    robot_description = ParameterValue(robot_description_substitution, value_type=str)
+    # 3) Готовим словарь
+    robot_description_param = {'robot_description': robot_description}
+
+    nodes = [
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[robot_description_param]
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen',
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_file],
+            output='screen'
+        ),
+    ]
+
+    shutdown_timer = TimerAction(
+        period=time_work,
+        actions=[
+            LogInfo(msg=f"Mission time ({time_work}s) completed. Shutting down..."),
+            Shutdown(reason='Mission complete')
+        ]
+    )
+
+    ld = LaunchDescription()
+
+    for node in nodes:
+        ld.add_action(node)
+    
+    ld.add_action(shutdown_timer)
+
+    return ld
