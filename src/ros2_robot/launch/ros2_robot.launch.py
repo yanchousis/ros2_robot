58b6184f24e65fa2f2c914eae07b2c0dd4fba087
bip <none>
2026-03-22 12:17:33 +0300

    slam & work 2.0

--- src/ros2_robot/launch/ros2_robot.launch.py
+++ src/ros2_robot/launch/ros2_robot.launch.py
@@ -30,9 +30,9 @@ def generate_launch_description():
         ),
 
         Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui',
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
             output='screen',
         ),
 
