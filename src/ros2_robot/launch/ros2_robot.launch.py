7c6f271257deec934d3a0e35e09365be1e06c95c
bip <none>
2026-03-22 12:02:44 +0300

    usb1 -> usb0

--- src/ros2_robot/launch/ros2_robot.launch.py
+++ src/ros2_robot/launch/ros2_robot.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
 
     serial_port_arg = DeclareLaunchArgument(
         'serial_port',
-        default_value='/dev/ttyUSB1',
+        default_value='/dev/ttyUSB0',
         description='Serial port for lidar'
     )
 
